2d8c616acec6292364e922a0fd0eb48f2609d8c8
SIDHARTHREDDY600 <sidharthreddy693@gmail.com>
2025-07-23 11:32:53 +0530

    Add files via upload

---
+++ TYPECASTING.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class TYPECASTING {
+    public static void main(String[] args) {
+        int n = 100;
+        double d = n;  // Implicit type casting (int to double)
+
+        System.out.println("Integer value: " + n);
+        System.out.println("Converted to double: " + d);
+    }
+}
