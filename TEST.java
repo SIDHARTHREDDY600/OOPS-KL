5b7980ce5ceb308a8142cf012a022f577ca3df26
SIDHARTHREDDY600 <sidharthreddy693@gmail.com>
2025-07-23 11:33:15 +0530

    Add files via upload

---
+++ TEST.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class TEST {
+	public static void main(String args[])
+	{
+	int x=5,y=7,c;
+	c=x+y;
+	System.out.println("addition is"+c);
+	for(c=0;c<=5;c++)
+	{
+	System.out.println("addition is "+c);
+	}
+	}
+}
